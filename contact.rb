010d6b169c89754d20ffbf1168f8bb61a66ec3ce
TannerEady <tanneready@shaw.ca>
2015-10-07 00:50:01 +0000

    Initial commit.
    Define methods to access a list of contacts in a csv file.

--- contact.rb
+++ contact.rb
@@ -1,9 +1,12 @@
+require 'csv'
 class Contact
- 
+
   attr_accessor :name, :email
 
   def initialize(name, email)
     # TODO: assign local variables to instance variables
+    @name = name
+    @email = email
   end
  
   def to_s
@@ -14,20 +17,61 @@ class Contact
   class << self
     def create(name, email)
       # TODO: Will initialize a contact as well as add it to the list of contacts
+      contacts = []
+      contacts[0] = contacts.object_id
+      contacts[1] = name
+      contacts[2] = email
+
+      CSV.open("contacts.csv", "a") do |csv|
+        csv << contacts
+      end
     end
  
     def find(term)
       # TODO: Will find and return contacts that contain the term in the first name, last name or email
+      index = false
+      File.open("contacts.csv", "r") do |file|
+        file.readlines.each do |line|
+          index = true if line.match(term.to_s)
+          puts line if index == true
+        end
+      end
+      index
     end
  
-    def all
+    def list
       # TODO: Return the list of contacts, as is
+     CSV.foreach('contacts.csv') do |row|
+      puts row.inspect
+      end
     end
     
     def show(id)
       # TODO: Show a contact, based on ID
+      index = false
+      File.open("contacts.csv", "r") do |file|
+        file.readlines.each do |line|
+          index = true if line.match(id.to_s)
+          puts line if index == true
+          index = false
+      end
     end
+  end
     
   end
  
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- contact_list.rb
+++ contact_list.rb
@@ -1,5 +1,31 @@
 require_relative 'contact'
 require_relative 'contact_database'
 
+class ContactList
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+
+def main_menu(command)
+  print "Please enter a command: "
+  command = gets.chomp.downcase
+   case command 
+   when 'help' then 
+    puts "Here is a list of available commands:"
+    puts "new - Create a new contact."
+    puts "list - List all contacts."
+    puts "show - Show a contact."
+    puts "find - Find a contact."
+  when 'new' then
+    prints "Please enter contact name: "
+    name = gets.chomp
+    prints "Please enter contact email address: "
+    email = gets.chomp
+    Contact.create(name, email)
+  when 'list' then Contact.list
+  when 'show' then Contact.show
+  when 'find' then Contact.find
+  end
+end
+end
+
+
